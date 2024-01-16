90e51d7e99507bc2e479dc7ff51461ca8602ebf2
E-Korsakova <evild3vilcat@gmail.com>
2024-01-16 12:29:19 +0300

    test

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,54 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    "airbnb", 
+    "airbnb/hooks", 
+    "plugin:react/jsx-runtime", 
+    "prettier"
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: "latest",
+    sourceType: "module"
   },
+  ignorePatterns: ['node_modules', 'dist', '.eslintrc.cjs', 'build'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'react', 'jsx-a11y', 'react-hooks', 'prettier', 'import'],
+  rules: {
+    "react/state-in-constructor": 0,
+    "jsx-a11y/label-has-associated-control": 0,
+    "indent": ["error", 2],
+    "prettier/prettier": "error",
+    "linebreak-style": [0, "unix"],
+    "quotes": ["error", "single"],
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": 0,
+    "import/no-unresolved": [2, { "caseSensitive": false }],
+    "react/jsx-filename-extension": [1, { "extensions": ['.js', ".jsx", '.ts', '.tsx'] }],
+    "import/order": [
+      2,
+      {
+        "groups": [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index"
+        ],
+        "newlines-between": "always"
+        }
+      ]
+    },
+    "settings": {
+      "version": "detect",
+      "import/resolver": {
+        "node": {
+          "extensions": [".js", ".jsx", ".ts", ".tsx"],
+          "moduleDirectory": ["node_modules", "src/"]
+        }
+      }
+    }
 }
